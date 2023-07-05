c3177532e8ed4fac1efed34977d652537b9a65a0
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-07-05 17:34:00 -0400

    fix
    change auth.js line 16

--- movie_api/auth.js
+++ movie_api/auth.js
@@ -17,6 +17,7 @@ module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false},
         (error, user, info) => {
+            console.log(error, user)
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
